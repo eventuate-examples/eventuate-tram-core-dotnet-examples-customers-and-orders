227f4e360058e5032d61441bfa54b707bd62b34f
Charanpreet Singh <[charansingh83sh@gmail.com]>
2020-10-26 17:29:14 +0530

    Changes to implement Eventually and generic class for Rest API

---
+++ ServiceCommon/Helpers/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServiceCommon.Helpers
+{
+    public static class Util
+    {
+        public static void Eventually(int iterations, int timeoutInMS, Action action)
+        {
+            for (int i = 1; i <= iterations; i++)
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (Exception)
+                {
+                    if (i == iterations)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(timeoutInMS);
+                }
+            }
+        }
+    }
+}
